297863c7f588f036fbc7a5b4e46027a4d3dbe505
J.D <shaefferjd@gmail.com>
2019-03-09 18:49:33 -0700

    lots of changes - getting email form ready with PHP

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import pic03 from '../images/pic03.jpg'
 import kipSite from '../images/kipSite.png'
 import kipController from '../images/controller.jpg'
 import gearCalc from '../images/GearCalc.png'
+import profile from '../images/profile.jpg'
+import resume from '../images/resume.pdf'
+import axios from 'axios'
+
+const API_PATH = 'http://localhost:8000/portfolio-website/api/contact/index.php';
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
+      mailSent: false,
+      error: null
+    }
+  }
+
+  handleFormSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+    axios({
+      method: 'post',
+      url: `${API_PATH}`,
+      headers: {'content-type': 'application/json'},
+      data: this.state
+    })
+    .then(result => {
+      this.setState({
+        mailSent: result.data.sent
+      })
+    })
+    .catch(error => this.setState({ error: error.message }));
+  }
+  
   render() {
 
     let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
@@ -32,38 +64,56 @@ class Main extends React.Component {
           <h3>A New Approach to Data Collection</h3>
           <p>Researching the ways that fun text-based adventures can be used as a data collection tool. It's currently in the development stages, but in the meantime, try out some of the beta versions of the games that have been made available so far.</p>
           <ul>
-            <li><a href="">A Dude In A Maze</a></li>
-            <li><a href="">Escape</a></li>
+            <li><a href='../games/dudeinamaze.html'>A Dude In A Maze</a></li> {/* both of these currently don't work - need to make them react components */}
+            <li><a href='../games/escape.html'>Escape</a></li>
           </ul>
           {close}
         </article>
 
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">About</h2>
-          <span className="image main"><img src={pic03} alt="" /></span>
-          <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
+          <h2 className="major">About & Resume</h2>
+          <span className="image main"><img src={profile} alt="" /></span>
+          <p>I'm currently pursuing a Computer Science bachelor's degree at Arizona State University. When I'm not studying I'm probably either reading Harry Potter or coding at my job at the Luminosity Lab, where I'm a software developer specializing in front-end web technologies and mobile app development. </p>
+          <p>If I'm not doing any of that I might be in the gym pumping wicked heavy iron, eating bricks, or hammering nails lololol</p>
+          <ul>
+            <li><a href={resume}>Resume</a></li>
+          </ul>
           {close}
         </article>
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
+          <form method="post" action=""> 
             <div className="field half first">
               <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
+              <input type="text" name="name" id="name"
+              value={this.state.name}
+              onChange={e=>this.setState({name: e.target.value})}/>
             </div>
             <div className="field half">
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
+              <input type="text" name="email" id="email"
+              value={this.state.email}
+              onChange={e=>this.setState({email: e.target.value})}/>
             </div>
             <div className="field">
               <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
+              <textarea name="message" id="message" rows="4"
+              value={this.state.message}
+              onChange={e=>this.setState({message: e.target.value})}
+              ></textarea>
             </div>
             <ul className="actions">
-              <li><input type="submit" value="Send Message" className="special" /></li>
+              <li><input type="submit" value="Send Message" className="special" // value should be Submit??
+              onClick={e=>this.handleFormSubmit(e)}/>
+              </li>
               <li><input type="reset" value="Reset" /></li>
             </ul>
+            <div>
+              {this.state.mailSent &&
+              <p>Thanks for the email!</p>
+              }
+            </div>
           </form>
           <ul className="icons">
             <li><a href="https://www.linkedin.com/in/j-d-s-0b5751132/" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
