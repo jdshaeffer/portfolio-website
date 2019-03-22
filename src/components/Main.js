458c35a5e0156f5c56d82d58d1249e240ccc9daf
J.D <shaefferjd@gmail.com>
2019-03-22 15:14:35 -0700

    got rid of twilio and php

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,43 +5,10 @@ import kipController from '../images/controller.jpg'
 import gearCalc from '../images/GearCalc.png'
 import profile from '../images/profile.jpg'
 import resume from '../images/resume.pdf'
-import axios from 'axios'
-
-const API_PATH = 'contact.test';
 
 class Main extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      email: '',
-      message: '',
-      mailSent: false,
-      error: null
-    }
-  }
-
-
-  handleFormSubmit = e => {
-    e.preventDefault();
-    axios({
-      method: 'post',
-      url: `${API_PATH}`,
-      headers: { 'content-type': 'application/json' },
-      data: this.state
-    })
-      .then(result => {
-        this.setState({
-          mailSent: result.data.sent
-        })
-      })
-      .catch(error => this.setState({ error: error.message }));
-  };
-  
   render() {
-
     let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
-
     return (
       <div ref={this.props.setWrapperRef} id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
 
@@ -87,33 +54,24 @@ class Main extends React.Component {
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name"
-              value={this.state.name}
-              onChange={e=>this.setState({name: e.target.value})}/>
+              />
             </div>
             <div className="field half">
               <label htmlFor="email">Email</label>
               <input type="text" name="email" id="email"
-              value={this.state.email}
-              onChange={e=>this.setState({email: e.target.value})}/>
+              />
             </div>
             <div className="field">
               <label htmlFor="message">Message</label>
               <textarea name="message" id="message" rows="4"
-              value={this.state.message}
-              onChange={e=>this.setState({message: e.target.value})}
               ></textarea>
             </div>
             <ul className="actions">
               <li><input type="submit" value="Send Message" className="special" // value should be Submit??
-              onClick={e=>this.handleFormSubmit(e)}/>
+              />
               </li>
               <li><input type="reset" value="Reset" /></li>
             </ul>
-            <div>
-              {this.state.mailSent &&
-              <p>Thanks for the email!</p>
-              }
-            </div>
           </form>
           <ul className="icons">
             <li><a href="https://www.linkedin.com/in/j-d-s-0b5751132/" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
