4fe1b1f9f0ddfa4efd1ef0a6735839958b32265e
jdshaeffer <shaefferjd@gmail.com>
2019-10-24 15:01:12 -0700

    edits to portfolio and changed background image

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import kipSite from '../images/kipSite.png'
 import kipController from '../images/controller.jpg'
-import gearCalc from '../images/GearCalc.png'
 import profile from '../images/profile.jpg'
 import resume from '../images/resume.pdf'
 
@@ -24,16 +23,10 @@ class Main extends React.Component {
           <span className="image main"><img src={kipController} alt="" /></span>
           <p>Spearheaded the creation of the mobile app that has become the working controller of the robot's movement and arm gestures. All done in React Native, along with the help of my supervisor, <a href="https://cadams.io/">Chase Adams</a>.</p>
           
-          <h3>The Gear Calculator - iOS App</h3>
-          <span className="image main"><img src={gearCalc} alt="" /></span> {/* it's way too big */}
-          <p>A huge stress for many thru-hikers is to keep their packs as light as possible. In order to alleviate the time-consuming activities of gear research and weighing, I built The Gear Calculator to let hikers see a huge selection of gear and its data all in one place. Think <a href="https://lighterpack.com">LighterPack</a>, only you don't have to manually input any weight or anything.</p>
-
-          <h3>A New Approach to Data Collection</h3>
-          <p>Researching the ways that fun text-based adventures can be used as a data collection tool. It's currently in the development stages, but in the meantime, try out some of the beta versions of the games that have been made available so far.</p>
-          <ul>
-            <li><a href='../games/dudeinamaze.html'>A Dude In A Maze</a></li> {/* both of these currently don't work - need to make them react components */}
+          {/* <ul>
+            <li><a href='../games/dudeinamaze.html'>A Dude In A Maze</a></li> 
             <li><a href='../games/escape.html'>Escape</a></li>
-          </ul>
+          </ul> */}
           {close}
         </article>
 
